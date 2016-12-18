a45a922290dd27c5c1cca797537a59f8e60b33c8
Eswar <eswar@eswarsmbpro.attlocal.net>
2016-12-18 17:10:34 -0600

    first commit

---
+++ src/test/java/com/org/maven1/TestNgMavenExampleTest.java
@@ -0,0 +1,20 @@
+package com.org.maven1;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenExampleTest {
+
+    @Test
+    public void exampleOfTestNgMaven() {
+
+        System.out.println("This is TestNG-Maven Example");
+
+    }
+    
+    @Test
+    public void exampleOfTestNgMaven2() {
+
+        System.out.println("This is TestNG-Maven Example 2");
+
+    }
+}
